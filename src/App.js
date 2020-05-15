33f27068e71521109f83cf8cf7c8b6b5f692c6a0
Morgan Page <admin@morganpage.tech>
2020-05-16 00:15:18 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactRogue from './ReactRogue.js';
+
+const App = () => (
+  <div className="App">
+    <ReactRogue width={40} height={40} tilesize={16} />
+  </div>
+);
+
+export default App;
